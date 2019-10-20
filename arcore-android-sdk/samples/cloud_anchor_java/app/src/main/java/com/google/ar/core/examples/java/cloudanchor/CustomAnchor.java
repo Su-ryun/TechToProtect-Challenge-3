b12063785d03720e65d909f035cd05845f46d66f
Su-ryun <dlee67@msudenver.edu>
2019-10-19 19:17:03 -0600

    Moar code.

---
+++ arcore-android-sdk/samples/cloud_anchor_java/app/src/main/java/com/google/ar/core/examples/java/cloudanchor/CustomAnchor.java
@@ -0,0 +1,10 @@
+package com.google.ar.core.examples.java.cloudanchor;
+
+public class CustomAnchor {
+
+    String assetName = "models/andy.obj";
+
+    public String assetName() {
+        return assetName;
+    }
+}
